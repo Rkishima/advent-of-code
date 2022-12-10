6417bc8d72d7c123680cf03e596989e7211d4f1a
RKishima <ricardokishima@hotmail.com>
2022-12-10 14:29:15 -0300

    day 3 complete

--- AdventOfCode/src/main/java/org/example/adventofcode/RucksackReorganization.java
+++ AdventOfCode/src/main/java/org/example/adventofcode/RucksackReorganization.java
@@ -5,15 +5,30 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RucksackReorganization {
-    public void sumOfThePriorities() throws IOException {
+    Map<Character, Integer> alphabeticMap;
+    Character[] alphabetic = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    public RucksackReorganization() {
+        alphabeticMap = new HashMap<>();
+        for (int i = 0; i < alphabetic.length; i++) {
+            alphabeticMap.put(alphabetic[i], i + 1);
+        }
+    }
+
+
+    public void sumOfThePriorities() throws IOException, URISyntaxException {
         int totalScore = 0;
-        File file = new File("D:\\Minhas coisas\\PROGRAMAÇÃO\\adventofcode\\rucksackReorganization.txt");
-        Scanner fileScanner = new Scanner(new File(file.toURI()));
+//        File file = new File("D:\\Minhas coisas\\PROGRAMAÇÃO\\adventofcode\\rucksackReorganization.txt");
+        URL url = ClassLoader.getSystemResource("rucksackReorganization.txt");
+        Scanner fileScanner = new Scanner(new File(url.toURI()));
 
         while (fileScanner.hasNext()) {
             String rucksack = fileScanner.nextLine();
@@ -25,20 +40,46 @@ public class RucksackReorganization {
             Character[] firstPackCharacters = ArrayUtils.toObject(charObjectArray1);
             Character[] secondPackCharacters = ArrayUtils.toObject(charObjectArray2);
 
-            for (int letterOfFirstPack = 1; letterOfFirstPack < firstPackCharacters.length; letterOfFirstPack++){
-                for (int letterOfSecondPack = 1; letterOfSecondPack < firstPackCharacters.length; letterOfSecondPack++){
-                    if (firstPackCharacters[letterOfFirstPack].equals(secondPackCharacters[letterOfSecondPack])){
-                        System.out.println(firstPackCharacters[letterOfFirstPack] + " is equals " +secondPackCharacters[letterOfSecondPack]);
-                        calculateprioritie(firstPackCharacters[letterOfFirstPack]);
-                    }
-                }
-            }
+            Optional<Character> matchChar = Arrays.stream(firstPackCharacters)
+                    .filter(new HashSet<>(List.of((secondPackCharacters)))::contains).findFirst();
+
+            System.out.println(matchChar);
+
+            totalScore += alphabeticMap.get(matchChar.get());
         }
+
         fileScanner.close();
     }
 
-    public int calculateprioritie(Character character){
+    public void sumOfTheGroupPriorities() throws IOException, URISyntaxException {
+        int totalScore = 0;
+        URL url = ClassLoader.getSystemResource("rucksackReorganization.txt");
+        Scanner fileScanner = new Scanner(new File(url.toURI()));
 
-        return -1;
+        while (fileScanner.hasNext()) {
+            String firstRucksack = fileScanner.nextLine();
+            String secondRucksack = fileScanner.nextLine();
+            String thirdRuckSack = fileScanner.nextLine();
+
+            char[] charObjectArray1 = firstRucksack.toCharArray();
+            char[] charObjectArray2 = secondRucksack.toCharArray();
+            char[] charObjectArray3 = thirdRuckSack.toCharArray();
+            Character[] firstPackCharacters = ArrayUtils.toObject(charObjectArray1);
+            Character[] secondPackCharacters = ArrayUtils.toObject(charObjectArray2);
+            Character[] thirdPackCharacter = ArrayUtils.toObject(charObjectArray3);
+
+            Optional<Character> matchChar = Arrays.stream(firstPackCharacters)
+                    .filter(new HashSet<>(List.of((secondPackCharacters)))::contains)
+                    .filter(new HashSet<>(List.of(thirdPackCharacter))::contains).findFirst();
+
+            totalScore += alphabeticMap.get(matchChar.get());
+        }
+        fileScanner.close();
+        System.out.println(totalScore);
     }
 }
+
+
+//v
+//l
+//r
--- AdventOfCode/src/test/java/adventofcode/RucksackReorganizationTest.java
+++ AdventOfCode/src/test/java/adventofcode/RucksackReorganizationTest.java
@@ -4,12 +4,17 @@ import org.example.adventofcode.RucksackReorganization;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class RucksackReorganizationTest {
     RucksackReorganization rucksackReorganization = new RucksackReorganization();
 
     @Test
-    public void sumOfThePriorities() throws IOException {
+    public void sumOfThePriorities() throws IOException, URISyntaxException {
         rucksackReorganization.sumOfThePriorities();
+    }
+
+    @Test public void sumOfTheGroupPriorities() throws IOException, URISyntaxException {
+        rucksackReorganization.sumOfTheGroupPriorities();
     }
 }
